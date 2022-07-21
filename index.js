c2889c73c114d90e5a3115d35ae23a9e3478bb73
Carlos Lugo <calugo71@gmail.com>
2022-07-21 14:20:06 -0400

    commit tree 6dd06533e439f955a79efe3ba0b556c8864baf4a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector('p').textContent='This is really cool!'
+    console.log("The DOM has loaded");
+});
+
+// document.querySelector('p').textContent='This is really cool!'
+// document.getElementById('text').textContent='This is really cool!'
